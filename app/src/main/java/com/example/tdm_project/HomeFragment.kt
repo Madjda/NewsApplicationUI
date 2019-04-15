95a0578e57a03975a58161f5c0f01f764b5503da
FatiMooni <ff_abdiche@esi.dz>
2019-04-15 19:43:16 +0200

    home updated

--- app/src/main/java/com/example/tdm_project/HomeFragment.kt
+++ app/src/main/java/com/example/tdm_project/HomeFragment.kt
@@ -1,21 +1,31 @@
 
 package com.example.tdm_project
 
+import android.annotation.SuppressLint
+import android.app.ActionBar
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
 import android.content.res.Configuration
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v4.app.Fragment
+import android.support.v7.widget.AppCompatButton
+import android.support.v7.widget.LinearLayoutCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.*
 import com.example.tdm_project.adapters.horizCardAdapter
 import com.example.tdm_project.adapters.vertCardAdapter
+import com.example.tdm_project.data.Topic
 import com.example.tdm_project.data.getList
 import com.example.tdm_project.data.news
 import java.util.*
@@ -28,6 +38,7 @@ class HomeFragment : Fragment() {
     lateinit var customHAdapter : horizCardAdapter
     lateinit var customVAdapter : vertCardAdapter
     var newsList = ArrayList<news>()
+    var topicsList = ArrayList<Topic>()
 
 
 
@@ -37,10 +48,12 @@ class HomeFragment : Fragment() {
 
 
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         //set the view
         rootView = inflater.inflate(R.layout.home_fragment, container, false)
 
+        GetTopics()
         newsList = getList()
 
 
@@ -131,4 +144,38 @@ class HomeFragment : Fragment() {
         setLocal(language)
     }
 
+    @SuppressLint("NewApi")
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun GetTopics(){
+        var layout = rootView.findViewById<LinearLayoutCompat>(R.id.Topics_buttom_holder)
+        var style = 0
+        var num = 0
+
+        topicsList.add(Topic("politics", rootView.context.resources.getDrawable(R.drawable.newspaper,null)))
+        topicsList.add(Topic("techno ", rootView.context.resources.getDrawable(R.drawable.tech_icon,null)))
+        topicsList.add(Topic("art", rootView.context.resources.getDrawable(R.drawable.art_icon,null)))
+        topicsList.add(Topic("art", rootView.context.resources.getDrawable(R.drawable.art_icon,null)))
+
+        topicsList.forEach {
+            var btn =  AppCompatButton(rootView.context)
+            btn.setCompoundDrawablesWithIntrinsicBounds(it.IconLink,null,null,null)
+            btn.setPadding(45,0,45,0)
+            btn.compoundDrawablePadding = 20
+            btn.maxWidth = 450
+            btn.minWidth = 350
+            btn.minHeight = 200
+            btn.text = it.title
+            btn.setTextColor(rootView.resources.getColor(R.color.white , null))
+            when (num){
+                0-> style = R.drawable.style_blue
+                1-> style = R.drawable.style_red
+                2-> style = R.drawable.style_green
+            }
+            num ++
+            if (num ==3)num =0
+
+            btn.background = rootView.resources.getDrawable(style,null)
+            layout.addView(btn)
+}
+}
 }
---
+++ app/src/main/java/com/example/tdm_project/data/Topic.kt
@@ -0,0 +1,8 @@
+package com.example.tdm_project.data
+
+import android.graphics.drawable.Drawable
+
+data class Topic(
+    var title : String ?,
+    var IconLink : Drawable
+)
